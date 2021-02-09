e702ec39acf9b177b0a80519117adfc04b677511
ZachHill21 <814055@mystma.org>
2021-02-09 17:56:16 +0000

    try 1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#WRITE YOUR CODE IN THIS FILE
+def findDoubles(w):
+    for i in range(0, len(w)):
+        if w[i] == "l":
+            return True 
+        else: 
+            return False
+print (findDoubles("log"))
